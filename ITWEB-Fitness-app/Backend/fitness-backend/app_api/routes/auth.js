675b07f8a15e62db924b1ca426a08c199ab29a4b
Christopher Bernold <christopherbernold@icloud.com>
2019-10-24 17:45:01 +0200

    Backend Authentication done

--- ITWEB-Fitness-app/Backend/fitness-backend/app_api/routes/auth.js
+++ ITWEB-Fitness-app/Backend/fitness-backend/app_api/routes/auth.js
@@ -2,7 +2,8 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
-const {registerValidation} = require('../models/Validation')
+const jwt = require('jsonwebtoken');
+const {registerValidation, loginValidation} = require('../models/Validation')
 
 //Validation
 
@@ -14,7 +15,6 @@ router.post('/register', async(req,res)=>{
 
     //Check if email is in db
     const emailExists = await User.findOne({email: req.body.email});
-
     if(emailExists) return res.status(400).send('Email already exists');
 
     //Hashing password
@@ -28,11 +28,30 @@ router.post('/register', async(req,res)=>{
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user: user.id});
 
     }catch(err){
         res.status(400).send(err);
     }
 });
+
+//login
+router.post('/login', async(req, res)=>{
+    //Data validation
+    const {error} = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Check if email is in db
+    const user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Email does not exist');
+    //check password
+    const passwordIsValid = await bcrypt.compare(req.body.password, user.password);
+    if(!passwordIsValid) return res.status(400).send('Wrong password');
+
+    //JWT token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+});
+
 module.exports = router;
 
