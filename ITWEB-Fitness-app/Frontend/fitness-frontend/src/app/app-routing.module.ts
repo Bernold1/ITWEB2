39a36069d3ea7404bd191af6aeea0cb78f5f33d5
Christopher Bernold <christopherbernold@icloud.com>
2019-11-06 16:37:00 +0100

    made a nice start page in the frontend!

---
+++ ITWEB-Fitness-app/Frontend/fitness-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+//Importing components
+import {LoginComponent} from './login/login.component'
+import {RegisterComponent} from './register/register.component'
+import{HomeComponent} from './home/home.component'
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component:HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent } 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
