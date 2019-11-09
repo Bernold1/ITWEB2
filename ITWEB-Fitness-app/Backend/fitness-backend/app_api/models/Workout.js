f653f9f6d09322cce7d630b18515dc3e231a0295
Christopher Bernold <christopherbernold@icloud.com>
2019-11-09 14:52:12 +0100

    backend fixed!

--- ITWEB-Fitness-app/Backend/fitness-backend/app_api/models/Workout.js
+++ ITWEB-Fitness-app/Backend/fitness-backend/app_api/models/Workout.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Exercise = require("./Exercise");
+const Exercise = require("./Exercise").schema;
 //inspired by https://mongoosejs.com/docs/populate.html
 
 const workoutSchema = Schema({
   name: { type: String, required: true },
   date: { type: Date, default: Date.now },
-  exercises: [{ type: Schema.Types.ObjectId, ref: "Exercise" }]
+  exercises: [Exercise]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
